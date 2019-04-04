00f8b00403374d3df0be28fa69c4502b59b1b68a
Teck9i9eteen <43444429+Teck9i9eteen@users.noreply.github.com>
2019-04-04 18:09:51 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+$(".Naruto").hide();
+$(".Sasuke").hide();
+$(".Kakashi").hide();
+$(".Boruto").hide();
+
+$("button").click(function() {
+    var TV = $(".TV").val();
+    var Character = $(".Character").val();
+    console.log(TV, Character);
+    
+    if (TV > 3 && Character === "Naruto") {
+        $(".Naruto").fadeIn();
+    } else if (TV < 3 && Character === "Naruto") {
+        $(".Sasuke").fadeIn();
+    } else if (TV > 3 && Character === "Boruto") {
+        $(".Kakashi").fadeIn();
+    } else if (TV < 3 && Character === "Boruto") {
+        $(".Boruto").fadeIn();
+    } else {
+        alert("You typed something that was not valid. Please try again");
+    }
+});
